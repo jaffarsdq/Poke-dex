9a3462c2321cfdc73fac1fec8c8f500a1e1e1d4f
Jaffar sadhiq <jaffarsdq007@gmail.com>
2023-08-09 22:59:15 +0530

    updated ui theme

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-//css import
 
+//css import
 import './navbar.css'
+//assets import
+import pokemonLogo from '../../assets/pokemonLogo.png'
 
 function Navbar() {
   return (
     <nav className='nav-wrapper'>
         <Link to={'/'} className="nav-link">
-            Poke Dex
+            <img src={pokemonLogo} alt="pokemon logo" className='nav-logo'/>
         </Link>
         <div className="nav-input-wrapper">
             <input 
--- src/components/pokeDetail/PokeDetail.jsx
+++ src/components/pokeDetail/PokeDetail.jsx
@@ -10,10 +10,11 @@ function PokeDetail({name, height, weight, image, types}) {
       <div className="detail-main">
         <img className='detail-img' src={image} alt="pokemon" />
         <h3 className='detail-name'>{name}</h3>
-        <h3 className='detail-height'>height : {height}</h3>
-        <h3 className='detail-weight'>weight : {weight}</h3>
+        <div className='detail-height'>Height : {height}</div>
+        <div className='detail-weight'>Weight : {weight}</div>
       </div>
       <div className="detail-type-wrapper">
+        <h6 className='detail-type-heading'>Type of poke :</h6>
         {types && types.map((t, i) => <h6 key={i} className='detail-type'>{t}</h6>)}
       </div>
     </div>
